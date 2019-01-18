a3fc2cc8effe485eee3915e17808588cb2baef6e
Youngsun <lsun@live.cn>
2019-01-19 01:04:40 +0800

    准备几个方法

--- Main.ts
+++ Main.ts
@@ -1,9 +1,33 @@
 class Main {
-    public Run(): void {
-        console.log('Hello Sokoban!');
+    private readonly canvas!: HTMLCanvasElement;
+    private readonly ctx!: CanvasRenderingContext2D;
+
+    constructor() {
+        this.canvas = document.createElement("canvas");
+        this.canvas.style.width = '400px';
+        this.canvas.style.height = '400px';
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext("2d")!;
+        this.Setup();
+    }
+
+    public Setup(): void {
+        setInterval(() => {
+            this.Update();
+            this.Show();
+        }, 40);
+
+    }
+
+    private Update(): void {
+    }
+
+    private Show(): void {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
 window.onload = () => {
-    new Main().Run();
+    new Main();
 };
--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 "use strict";
 class Main {
-    Run() {
-        console.log('Hello Sokoban!');
+    constructor() {
+        this.canvas = document.createElement("canvas");
+        this.canvas.style.width = '400px';
+        this.canvas.style.height = '400px';
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext("2d");
+        this.Setup();
+    }
+
+    Setup() {
+        setInterval(() => {
+            this.Update();
+            this.Show();
+        }, 40);
+    }
+
+    Update() {
+    }
+
+    Show() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 window.onload = () => {
-    new Main().Run();
+    new Main();
 };
 //# sourceMappingURL=app.js.map
